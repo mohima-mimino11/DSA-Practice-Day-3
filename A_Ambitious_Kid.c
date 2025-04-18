394b7be678fa3446ff67088aa9804e0ded955759
mohima-mimino11 <mohimahoque22@gmail.com>
2025-04-18 17:19:25 +0600

    More practice problem added

--- A_Ambitious_Kid.c
+++ A_Ambitious_Kid.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-int main(){
-  int n;
-  scanf("%d", &n);
-  long long int a[n];
-  for(int i=0; i < n; i++){
-    scanf("%lld", &a[i]);
-  }
-  return 0;
+#include <math.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    int min_num = 100000 + 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+
+        if (x < 0)
+        {
+            x = x * (-1);
+        }
+        // x = abs(x) ;
+
+        if (x < min_num)
+        {
+            min_num = x;
+        }
+    }
+
+    printf("%d\n", min_num);
 }
